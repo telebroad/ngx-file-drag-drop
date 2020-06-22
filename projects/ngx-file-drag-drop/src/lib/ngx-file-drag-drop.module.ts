4ca1c196458667efe3fe42336023451ed628691b
Meir Blumenfeld <33358938+EFFKAY2@users.noreply.github.com>
2020-06-21 23:48:05 -0400

    initial chips

--- projects/ngx-file-drag-drop/src/lib/ngx-file-drag-drop.module.ts
+++ projects/ngx-file-drag-drop/src/lib/ngx-file-drag-drop.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { NgxFileDragDropComponent } from './ngx-file-drag-drop/ngx-file-drag-drop.component';
 import { CommonModule } from '@angular/common';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+
 
 
 
 @NgModule({
   declarations: [NgxFileDragDropComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatChipsModule,
+    MatIconModule
   ],
   exports: [NgxFileDragDropComponent]
 })
