f78474cb5fff8c2f76783a825b65653eade8d5f5
Meir Blumenfeld <33358938+EFFKAY2@users.noreply.github.com>
2020-06-30 01:02:44 -0400

    file-size

--- projects/ngx-file-drag-drop/src/lib/ngx-file-drag-drop.module.ts
+++ projects/ngx-file-drag-drop/src/lib/ngx-file-drag-drop.module.ts
@@ -3,17 +3,18 @@ import { NgxFileDragDropComponent } from './ngx-file-drag-drop/ngx-file-drag-dro
 import { CommonModule } from '@angular/common';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
+import { BytePipe } from './byte.pipe';
 
 
 
 
 @NgModule({
-  declarations: [NgxFileDragDropComponent],
+  declarations: [NgxFileDragDropComponent, BytePipe],
   imports: [
     CommonModule,
     MatChipsModule,
     MatIconModule
   ],
-  exports: [NgxFileDragDropComponent]
+  exports: [NgxFileDragDropComponent, BytePipe]
 })
 export class NgxFileDragDropModule { }
